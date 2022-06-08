00686461e40fdc122285abbaa3201cc3c3907b30
Starchild1337 <55667234+Starchild1337@users.noreply.github.com>
2022-06-08 20:17:00 +0400

    Updates

---
+++ precache-manifest.58d878ebb51111134caddbdfd35d0e5b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c571c938b9db33e62d2130564ea3262",
+    "url": "/crowdfunding/index.html"
+  },
+  {
+    "revision": "db4d23ee1b013a263401",
+    "url": "/crowdfunding/static/css/main.58be6a55.chunk.css"
+  },
+  {
+    "revision": "f0213b51288fdd413f30",
+    "url": "/crowdfunding/static/js/2.6648d413.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crowdfunding/static/js/2.6648d413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db4d23ee1b013a263401",
+    "url": "/crowdfunding/static/js/main.43ac5af4.chunk.js"
+  },
+  {
+    "revision": "50e380bdb0c2015e0b58",
+    "url": "/crowdfunding/static/js/runtime-main.fbe725f5.js"
+  },
+  {
+    "revision": "a942b4d803063af7f1395db58298636d",
+    "url": "/crowdfunding/static/media/close-modal.a942b4d8.svg"
+  },
+  {
+    "revision": "7f6e2a1747ed58c8d628a9bfd1f28f86",
+    "url": "/crowdfunding/static/media/hero-desktop.7f6e2a17.jpg"
+  },
+  {
+    "revision": "5b0777273803774337e5b001c3473e43",
+    "url": "/crowdfunding/static/media/icon-bookmark.5b077727.svg"
+  },
+  {
+    "revision": "b2fbba35d9fbada00f5fa91fc7f0dab5",
+    "url": "/crowdfunding/static/media/icon-check.b2fbba35.svg"
+  },
+  {
+    "revision": "7b0ab1a360478af7171ed5783343789d",
+    "url": "/crowdfunding/static/media/icon-close-menu.7b0ab1a3.svg"
+  },
+  {
+    "revision": "f8b2673b95669637f13c170cc659070c",
+    "url": "/crowdfunding/static/media/icon-hamburger.f8b2673b.svg"
+  },
+  {
+    "revision": "17fbdfc2b65dde3d22652fd0e0fa60b2",
+    "url": "/crowdfunding/static/media/logo-mastercraft.17fbdfc2.svg"
+  },
+  {
+    "revision": "213a6fb994faca9736132fa9348b8700",
+    "url": "/crowdfunding/static/media/logo.213a6fb9.svg"
+  }
+]);
